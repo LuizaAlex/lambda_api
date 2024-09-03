de2dbcd38728046eb12de4698a0fdf1db66b4805
LuizaAlex <poteca.luiza@gmail.com>
2024-09-03 16:36:54 +0300

    .

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,12 +18,10 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		// resultMap.put("statusCode", 200);
-		// resultMap.put("message", "Hello from Lambda");
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	Map<String, Object> resultMap = new HashMap<>();
+    resultMap.put("statusCode", 200);
+    resultMap.put("body", "{\"message\":\"Hello from Lambda\"}");
+    resultMap.put("headers", Collections.singletonMap("Content-Type", "application/json"));
+    return resultMap;
 	}
 }
